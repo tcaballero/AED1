8d9456fe5902597d4a5f34c90000d33f4cf30f90
Tomás Caballero <tomycaballero95@gmail.com>
2017-06-05 22:57:29 -0300

    Init commit

---
+++ taller-strings/ejercicios.h
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool anagramas(string s1, string s2);
+string darVuelta(string s);
+vector<string> split(string s, char delim);
+string darVueltaPalabra(string s);
+bool subsecuencia(string s1, string s2);
+vector<vector<string> > agruparAnagramas(vector<string> v);
+bool esPalindromo(string s);
+bool tieneRepetidos(string s);
+string rotar(string s, int j);
+string darVueltaK(string s, int k);
