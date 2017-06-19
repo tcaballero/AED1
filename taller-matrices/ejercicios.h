083331fb919aa6f9c1305c69caf3549021759f85
Tomás Caballero <tomycaballero95@gmail.com>
2017-06-19 00:19:16 -0300

    First commit

---
+++ taller-matrices/ejercicios.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+#include <sstream>
+
+using namespace std;
+
+void trasponer(vector<vector<int> > &m);
+vector<vector<int> > multiplicar(vector<vector<int> > m1, vector<vector<int> > m2);
+vector<vector<int> > promediar(vector<vector<int> > m);
+int contarPicos(vector<vector<int> > m);
+int hayAmenaza(vector<vector<int> > m);
