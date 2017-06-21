9fc81b0fc61e5603ea6b4c1f784a60a05ff4da21
Tomás Caballero <tomycaballero95@gmail.com>
2017-06-21 16:35:27 -0300

    Init commit

---
+++ practica-9/ejercicios.h
@@ -0,0 +1,12 @@
+#ifndef SRC_EJERCICIOS_H_
+#define SRC_EJERCICIOS_H_
+
+#include<vector>
+#include <utility>
+using namespace std;
+
+vector<int> mesetaMasLarga(vector<int> v);
+int infrasecuenciaMayor(vector<int> v);
+vector<vector<int> > productoCartesiano(vector<int> v1, vector<int> v2);
+
+#endif /* SRC_EJERCICIOS_H_ */
