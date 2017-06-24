77c00bdd12af79f918c61fa02ab4e767e1cc17f7
Tomás Caballero <tomycaballero95@gmail.com>
2017-06-24 19:14:29 -0300

    Hasta ejercicio 12
    Cambios en infrasecuenciaMayor

--- practica-9/ejercicios.h
+++ practica-9/ejercicios.h
@@ -3,10 +3,22 @@
 
 #include<vector>
 #include <utility>
+#include <algorithm>
+#include <string>
 using namespace std;
 
 vector<int> mesetaMasLarga(vector<int> v);
-int infrasecuenciaMayor(vector<int> v);
+vector<vector<int> > secuenciasConCerosNueves(int n);
+vector<int> infrasecuenciaMayor(vector<int> v);
 vector<vector<int> > productoCartesiano(vector<int> v1, vector<int> v2);
+int distanciaHamming(vector<int> v1, vector<int> v2);
+int quickSelect(vector<int> v, int k);
+int indiceSuma(vector<int> v);
+vector<vector<int> > comprimir(vector<int> v);
+bool esSubcadena(string s1, string s2);
+string subcadenaComunMasLarga(string s1, string s2);
+string subcadenaOrdenadaDeLargoN(string s, int l);
+string darVueltaPalabra(string s);
 
 #endif /* SRC_EJERCICIOS_H_ */
+
