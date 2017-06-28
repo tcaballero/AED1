30d33d160d187b1a51b55018e0c6f3ac7e186aac
Tomás Caballero <tomycaballero95@gmail.com>
2017-06-27 23:27:11 -0300

    Init commit

---
+++ practica-10/header.h
@@ -0,0 +1,16 @@
+#ifndef SRC_PRACTICA10_H_
+#define SRC_PRACTICA10_H_
+
+#include<vector>
+#include <utility>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+typedef vector<vector<int> > matriz;
+
+int determinante(matriz m);
+matriz producto(matriz m1, matriz m2);
+
+#endif /* SRC_PRACTICA10_H_ */
